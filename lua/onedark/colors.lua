00eb0129fa6b1a0bafbc61fb79b27a24a806e242
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-09-02 15:28:47 +0530

    feat: variable_builtin highlight

--- lua/onedark/colors.lua
+++ lua/onedark/colors.lua
@@ -39,7 +39,8 @@ function M.setup(config)
     syntax = {
       variable = "#e06c75",
       operator = "#56b6c2",
-      property = "#56b6c2"
+      property = "#56b6c2",
+      variable_builtin = "#e5c07b"
 
       --
     },
