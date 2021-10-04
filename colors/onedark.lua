bde0d411b4acca5d998909d8e6c0cdaae4b08f1d
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-10-04 16:38:08 +0530

    fix: reload with 'colorscheme' command

--- colors/onedark.lua
+++ colors/onedark.lua
@@ -5,3 +5,5 @@ package.loaded["onedark.util"] = nil
 package.loaded["onedark.colors"] = nil
 package.loaded["onedark.theme"] = nil
 package.loaded["onedark.config"] = nil
+
+require("onedark").setup()
