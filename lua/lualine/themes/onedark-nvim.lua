526bc44a438e881ea1522ce1ffa97791b0dc3641
samrath2007 <samrath1324@gmail.com>
2021-10-31 10:51:46 +0530

    feat(themes): add lualine theme

---
+++ lua/lualine/themes/onedark-nvim.lua
@@ -0,0 +1,58 @@
+local config = require("onedark.config")
+local colors = require("onedark.colors").setup(config)
+local util = require("onedark.util")
+
+local onedark = {}
+
+onedark.normal = {
+  a = { bg = colors.blue, fg = colors.black },
+  b = { bg = colors.fg_gutter, fg = colors.blue },
+  c = { bg = colors.bg_statusline, fg = colors.fg_sidebar },
+}
+
+onedark.insert = {
+  a = { bg = colors.green, fg = colors.black },
+  b = { bg = colors.fg_gutter, fg = colors.green },
+}
+
+onedark.command = {
+  a = { bg = colors.yellow, fg = colors.black },
+  b = { bg = colors.fg_gutter, fg = colors.yellow },
+}
+
+onedark.visual = {
+  a = { bg = colors.purple, fg = colors.black },
+  b = { bg = colors.fg_gutter, fg = colors.purple },
+}
+
+onedark.replace = {
+  a = { bg = colors.red, fg = colors.black },
+  b = { bg = colors.fg_gutter, fg = colors.red },
+}
+
+onedark.inactive = {
+  a = { bg = colors.bg_statusline, fg = colors.blue },
+  b = { bg = colors.bg_statusline, fg = colors.fg_gutter, gui = "bold" },
+  c = { bg = colors.bg_statusline, fg = colors.fg_gutter },
+}
+
+if vim.o.background == "light" then
+  for _, mode in pairs(onedark) do
+    for _, section in pairs(mode) do
+      if section.bg then
+        section.bg = util.getColor(section.bg)
+      end
+      if section.fg then
+        section.fg = util.getColor(section.fg)
+      end
+    end
+  end
+end
+
+if vim.g.onedark_lualine_bold then
+  for _, mode in pairs(onedark) do
+    mode.a.gui = "bold"
+  end
+end
+
+return onedark
